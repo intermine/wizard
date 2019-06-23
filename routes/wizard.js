d445de536d7037df0ff684c084e3c73f0305aa8d
Yo Yehudi <yochannah@gmail.com>
2019-06-23 15:36:30 +0100

    make dynamic breadcrumbs

--- routes/wizard.js
+++ routes/wizard.js
@@ -11,6 +11,10 @@ router.get('/upload', function(req, res, next) {
   res.render(templatePath + 'upload');
 });
 
+router.get('/map-columns', function(req, res, next) {
+  res.render(templatePath + 'mapColumns');
+});
+
 router.get('/supplementary-data', function(req, res, next) {
   res.render(templatePath + 'supplementary');
 });
@@ -20,7 +24,7 @@ router.get('/mine-config', function(req, res, next) {
   res.render(templatePath + 'config');
 });
 
-/* Set name, licence, privacy */
+/* final step before we start the build */
 router.get('/review', function(req, res, next) {
   res.render(templatePath + 'review');
 });
