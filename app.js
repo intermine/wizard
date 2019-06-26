ed82d8aebcb2cc03cf7d790697d0484a636d56b6
Yo Yehudi <yo@intermine.org>
2019-06-26 11:42:46 +0100

    add docs and dev route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var wizardRouter = require('./routes/wizard');
+var devRouter = require('./routes/dev');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/wizard', wizardRouter);
+app.use('/dev', devRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
