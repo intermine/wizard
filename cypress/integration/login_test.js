35d41da2bc6c75c90a28e5746ef92e433feaf643
Ankit Kumar <ankits.kvmdam@hotmail.com>
2019-10-09 01:20:45 +0530

    add login test

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -1,9 +1,12 @@
 describe("Login Test", function(){
     before(function(){
+        // First we have to seed the database with one user details
+        // After the we are able to perform successful login test.
         cy.visit("localhost:9992/register");
         cy.fixture("login/register_user.json").then(function(obj){
             cy.feedRegistrationDetails(obj);
         });
+
         cy.get("#registerForm > .back")
             .click();
     })
@@ -25,5 +28,22 @@ describe("Login Test", function(){
         cy.get("#signinForm > .back")
             .click();
         cy.location('pathname').should('eq', '/wizard/upload');
+
+        // After login we have to logout
+        cy.visit("http://localhost:9992/users/profile")
+        cy.get("#logout").click()
+        cy.location('pathname').should('eq', '/register');
     })
+
+    // Check for unsuccessful login
+    it("Successful Login", function(){
+        cy.fixture("login/failure.json").then(function(obj){
+            cy.feedLoginDetails(obj)
+        });
+
+        cy.get("#signinForm > .back")
+            .click();
+        cy.get("#alertbox").should("contain", "Wrong credentials");
+    })
+
 })
