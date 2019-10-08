a4858bd40d73281308bb54e633e59ffe98b473f6
Ankit Kumar <ankits.kvmdam@hotmail.com>
2019-10-09 01:21:37 +0530

    command add for tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+// fill registration fields
 Cypress.Commands.add("feedRegistrationDetails", (obj)=>{
     cy.get("#register-email")
         .type(obj.email);
@@ -13,9 +14,26 @@ Cypress.Commands.add("feedRegistrationDetails", (obj)=>{
         .type(obj.confirm_password);
 })
 
+// fill login fields
 Cypress.Commands.add("feedLoginDetails", (obj) =>{
     cy.get("#signin-email")
         .type(obj.email);
     cy.get("#signin-password")
         .type(obj.password);
+})
+
+// check alertbox is in viewport or not
+Cypress.Commands.add("checkInViewPort", (element, inViewPort)=>{
+    const width = Cypress.$(cy.state('window')).width()
+    const alertbox_dimension = element.getBoundingClientRect()
+
+    // true means check if alertbox is in viewport
+    if(inViewPort === true){
+        // Alertbox left is the start point. If alertbox is in the viewport
+        // then the value of alertbox left is not greater than innerHTML width - alertbox width.
+        expect(alertbox_dimension.left).not.to.be.greaterThan(width - alertbox_dimension.width)
+    } else {
+        // false means check if alertbox is not in viewport
+        expect(alertbox_dimension.left).to.be.greaterThan(width - alertbox_dimension.width)
+    }
 })
